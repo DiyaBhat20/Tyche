61ca8980fc2c49ee06a922db27db9ec29044db61
Diya Bhat <diyabhat427@gmail.com>
2024-10-01 23:43:05 +0530

    Home Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
   return (
     <nav className="bg-black px-4 pb-2 flex justify-between items-center">
       <div className="text-white">
-        <h1 className="font-bold font-jeju text-[50px]">Tyche</h1>
-        <p className="font-jaldi text-[20px]">“A smile is a curve that sets everything straight.”</p>
-        <p className="font-jaldi text-[20px] text-[#3373A6]">Phyllis Diller</p>
+        <h1 className="font-bold font-jeju md:text-[50px] text-[40px]">Tyche</h1>
+        <p className="font-jaldi text-[15px] sm:text-[20px]">“A smile is a curve that sets everything straight.”</p>
+        <p className="font-jaldi text-[15px] sm:text-[20px] text-[#3373A6]">Phyllis Diller</p>
       </div>
-      <div className="flex flex-col items-end text-white font-jeju text-[20px]">
+      <div className="flex flex-col items-end text-white font-jeju text-[15px] sm:text-[20px]">
       <p>{getCurrentDate()}</p>
       <p>Thank you for being a client since 2019</p>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,17 +5,43 @@ const HomePage = () => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       
-      <div className="mt-8 flex justify-center text-white">
-        <input
+      <div className="mt-2 flex justify-center text-white">
+        <input               //Search Bar
           type="text"
           placeholder="Search here..."
-          className="bg-black border border-blue-600 p-2 border-2 focus:border-blue-600" style={{ width: '1000px' }} // Increased width
+          className="bg-black border border-blue-600 p-2 border-2 focus:border-blue-600 w-[80%]"  // Increased width
         />
       </div>
 
-      <div>
-        
+      <div className="mt-8 bg-[#131010] text-white w-[80%] mx-auto border-l-8 border-[#FFAB3A] px-40">
+        <div className="font-jeju">
+          <p className="text-[40px] pt-4">Stay informed with Tyche</p>
+          <p className="text-[40px]">virtual events.</p>
+          <p className="text-[22px] pt-3">Attend webinars that help to make sense of the current
+          market environment and see how Tyche can help.</p>
+          <div className="flex justify-between ">
+          <p className="text-[40px] py-4 text-[#FFAB3A]">SMNR</p>
+          <button className="text-white text-[25px] h-16 px-3 border-2 border-white hover:bg-[#242121]">GET STARTED</button>
+          </div>
+        </div>
       </div>
+
+      <div className="mt-8 pb-3 flex justify-between items-center bg-[#131010] text-white w-[90%] mx-auto border-l-8 border-[#FFAB3A] px-40">
+        <div>
+        <p className="text-[50px]">Tyche</p>
+        <p className="text-[30px]">BRIEFS</p>
+        </div>
+        <div>
+        <p className="text-[25px]">Market-leading Intelligence, News, and</p>
+        <p className="text-[25px]">Data delivered directly to your inbox</p>
+        </div> 
+        <div>
+        <p className="text-[20px]">Subscribe for Free</p>
+        <p className="text-[40px] text-[#FFAB3A]">BRIEF</p>
+        </div>                
+      </div>      
+
+
     </div>
   );
 }
